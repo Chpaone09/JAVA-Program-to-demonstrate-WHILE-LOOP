ac4a8168fa73b46341da3ab268ccde874c078ea3
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-13 02:56:48 +0530

    Program to demonstrate WHILE LOOP in java

---
+++ WhileLoop.java
@@ -0,0 +1,21 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate WHILE LOOP in java.
+Author  : Chpaone09®
+Date    : Oct 12, 2020
+
+************************************************************
+*/
+
+public class WhileLoop{
+    public static void main(String[] args) {
+        int i =1;
+        System.out.print("\n\n\t Demonstration of WHILE LOOP in JAVA >> ");
+        while(i<=5){
+            System.out.print(" "+(i++));
+        }
+    }
+    }
+    
+    
